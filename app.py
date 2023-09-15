b7c2a44f4617c32477c3207f1883c7d668322228
KierWaters <kwaters_97@hotmail.com>
2023-09-15 02:25:49 +0100

    Implementing the add function.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, request, render_template, url_for
 import json
 
 app = Flask(__name__)
@@ -12,6 +12,31 @@ def load_posts():
 def update_posts(posts):
     with open('posts.json', 'w') as file:
         json.dump(posts, file, indent=4)
+
+
+blog_posts = load_posts()
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
+
+        new_post = {
+            'id': len(blog_posts) + 1,
+            'author': author,
+            'title': title,
+            'content': content
+        }
+
+        blog_posts.append(new_post)
+        update_posts(blog_posts)  # Update the JSON file
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 @app.route('/')
