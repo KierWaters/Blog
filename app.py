980a0d51a8e4d0fa3abfea75872b18bcf1fd3128
KierWaters <kwaters_97@hotmail.com>
2023-09-15 01:53:01 +0100

    Phase one of my blog application

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
+def load_posts():
+    with open('posts.json', 'r') as file:
+        return json.load(file)
+
+
+def update_posts(posts):
+    with open('posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    posts = load_posts()
+    return render_template('index.html', posts=posts)
 
 
 if __name__ == '__main__':
